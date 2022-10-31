4367d5af63e834c787ad8cb43b576073ee8b241a
henrique-temponi <tanzenheavely@gmail.com>
2022-10-31 12:14:27 -0300

    Ajust javascript

--- lista.js
+++ lista.js
@@ -1,4 +1,69 @@
-let bancolista =  JSON.parse(localStorage.getItem("lista"));
+mercados = [
+    {
+        "id": 1,
+        "nome":"Mercado A",
+    },
+    {
+        "id": 2,
+        "nome":"Mercado B",
+    },
+    {
+        "id": 3,
+        "nome":"Mercado C",
+    }
+]
 
-//Esse é o exemplo do banco de dados que tive, secaso tiver uma ideia diefrente que te ajude você me fala.
+produtos = [
+    {
+        "id": 1,
+        "nome":"Produto A",
+    },
+    {
+        "id": 2,
+        "nome":"Mercado B",
+    },
+    {
+        "id": 3,
+        "nome":"Mercado C",
+    }
+]
+
+estoque = [    
+    {
+        "produto_id": 1,
+        "mercado_id": 1,
+        "preco": 10
+    },
+    {
+        "mercado_id": 1,
+        "produto_id": 2,
+        "preco": 11
+    },
+    {
+        "mercado_id": 1,
+        "produto_id": 3,
+        "preco": 9
+    },
+    {
+        "mercado_id": 2,
+        "produto_id": 1,
+        "preco": 10
+    },
+    
+    {
+        "mercado_id": 1,
+        "produto_id": 3,
+        "preco": 10
+    },
+    {
+        "mercado_id": 3,
+        "produto_id": 3,
+        "preco": 10
+    },
+    {
+        "mercado_id": 3,
+        "produto_id": 1,
+        "preco": 10
+    }
+]
 
