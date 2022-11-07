7944281a0cbb3c8ef9101f1e3a36079b64dbca1b
henrique-temponi <tanzenheavely@gmail.com>
2022-11-07 12:36:31 -0300

    Implementa metdodo de carregar dados

--- lista.js
+++ lista.js
@@ -1,11 +1,20 @@
 import * as banco from "./banco.js";
 
 function carregarDados() {
-    console.log(banco.carregarMercados());
+    // console.log(banco.carregarMercados());
+
+    let dadosMercado = document.getElementById("cadastrarMercardo");
+    let listDeMercados = banco.carregarMercados();
+
+    listDeMercados.forEach(e=> {
+        dadosMercado.innerHTML =  dadosMercado.innerHTML + `<option value="${e.idMercado}">${e.nomeMercado}</option>`
+    });
 }
 
 
 
+
+ 
 window.onload = () => {
     
     carregarDados();
